8a7ad4d13ac6b28ac9d5bf8720940323e215110d
Pimpus <milhan.verschelle@gmail.com>
2021-12-21 18:23:32 +0100

    modif filter

--- src/screens/Filters.js
+++ src/screens/Filters.js
@@ -7,8 +7,9 @@ export default function Filters()  {
         return (
             <div className="filters">
                 <Header />
-            <img className="filters-galaxy" src={Galaxy} alt="galaxy"></img>
-            <img className="filters-galaxy2" src={Galaxy} alt="galaxy"></img>  
+                <img className="filters-galaxy" src={Galaxy} alt="galaxy"></img>
+                <img className="filters-galaxy2" src={Galaxy} alt="galaxy"></img>
+                
                  
             </div>
         )
