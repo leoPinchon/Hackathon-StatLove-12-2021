5ac6d2c1e41ddb27d5400374cf735f6314c77852
MUNSCH <thomas-munsch_student2021@wilder.school>
2021-12-21 14:31:42 +0100

    Added first routes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
-function App() {
+import Chat from './components/Chat'
+import Filters from './components/Filters'
+import Header from './components/Header'
+import Home from './components/Home'
+
+const App = () => {
 
   return (
     <div className="App">
-     
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/filters" element={<Filters />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
     </div>
   );
 }
