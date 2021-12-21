fa88249d81c1ab76d4922c55bd385d96940faff6
Pinchon Léo <leopinchonspa@gmail.com>
2021-12-21 17:16:51 +0100

    création de la carte abdou

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,36 @@
-export default function Cards() {
+import abdou from "../assets/abdou.jpg";
+import "../styles/Cards.css";
+// import {Helmet} from "react-helmet";
+
+
+
+export default function Cards(props) {
   return (
-    <div>
+  
+    <div className="card-section">
       <div className="card">
         <div className="card-content">
           <div className="card-img-box">
-            <img src="#" alt="profil" />
+            <img src={abdou} alt="profil" />
           </div>
           <div className="card-content-box">
             <h3>
               Luke Skywalker
-              <br>
-                <span>Human</span>
-              </br>
+              <br />
+              <span>Human</span>
             </h3>
           </div>
+        
         </div>
+        <ul className="card-info">
+            <li>Genre: Male</li>
+            <li>Taille : 1.72cm</li>
+            <li >Résidence: Tatooine</li>
+          </ul>
       </div>
     </div>
+
+
   );
+
 }
