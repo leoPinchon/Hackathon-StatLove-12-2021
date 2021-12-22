3d4ba9a71f545c7455fa312608ba550c68a5e7e4
Pinchon Léo <leopinchonspa@gmail.com>
2021-12-22 04:16:33 +0100

    ajout dev leo

--- src/components/Filtre.js
+++ src/components/Filtre.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
+import Cards from './Cards'
 
-const Filtre = ({select}) => {
+const Filtre = ({select, setCharactersTabFiltered}) => {
 
     const [characters, setCharacters] = useState([])
+    
+    const [valueFiltered, setValuefiltered] = useState('')
+    
 
-    useEffect(() => {
+        useEffect(() => {
         const getData = () => {
             fetch(`https://miadil.github.io/starwars-api/api/all.json`)
             .then((res) => res.json())
@@ -12,14 +16,25 @@ const Filtre = ({select}) => {
         }
         getData()
     }, [])
-    
+     
         let selectedFilter = characters.reduce((acc, character) =>
         acc.includes(character[select]) ? acc : acc.concat(character[select]),
         [])
     
+        // filtrer le tableau
+        const dataValueFunc = (e) => setValuefiltered(e.target.value)
+        
+
+        const tabCharactersFiltered = characters.filter(character => character[select]===valueFiltered )
+        useEffect(() =>
+        console.log('filtre')||
+        setCharactersTabFiltered(tabCharactersFiltered),[valueFiltered]
+        )
+        
+
     return (
         <div>
-            <select name={select}>
+            <select name={select} value={valueFiltered} onChange={dataValueFunc}>
                 <option value="">{select}...</option>
                 {selectedFilter.map((data) => {
                 return <option value={data}>{data}</option>
