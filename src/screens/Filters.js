3e1a58031fe8a12cfe314d3d0a98852accf2a871
Yannick Lanteri <yannick-lanteri_student2021@wilder.school>
2021-12-22 04:34:40 +0100

    updates

--- src/screens/Filters.js
+++ src/screens/Filters.js
@@ -2,14 +2,14 @@ import Header from "../components/Header"
 import Galaxy from "../assets/galaxy01.png"
 import Filtre from "../components/Filtre"
 import Chat from "./Chat"
-
+import Home from "./Home"
 
 import "../styles/Filter.css"
 
 export default function Filters()  {
 
 
-
+        console.log(Home())
 
         return (
             <div className="filters">
@@ -17,6 +17,7 @@ export default function Filters()  {
                 <img className="filters-galaxy" src={Galaxy} alt="galaxy"></img>
                 <img className="filters-galaxy2" src={Galaxy} alt="galaxy"></img> 
                 <div className="filter-box">
+                <div className="title-filter"><h2>Bienvenue :</h2></div>
                 <div className="title-filter"><h2>Choisissez vos filtres :</h2></div> 
                     <div className="holder-filter-container">
                         <div className="filter-container">
