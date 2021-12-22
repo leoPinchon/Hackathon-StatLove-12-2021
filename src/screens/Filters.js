16416dbd1330d806822f5adc870749fee29c3892
Pimpus <milhan.verschelle@gmail.com>
2021-12-22 12:43:53 +0100

    modif style filter & timer

--- src/screens/Filters.js
+++ src/screens/Filters.js
@@ -22,27 +22,24 @@ export default function Filters()  {
                 <img className="filters-galaxy" src={Galaxy} alt="galaxy"></img>
                 <img className="filters-galaxy2" src={Galaxy} alt="galaxy"></img> 
                 <div className="filter-box">
-                <div className="title-filter"><h2>Bienvenue :</h2></div>
-                <div className="title-filter"><h2>Choisissez vos filtres :</h2></div> 
+                    <div className="title-filter"><h2>Bienvenue :</h2></div>
+                    <div className="title-filter"><h2>Choisissez vos filtres :</h2></div> 
                     <div className="holder-filter-container">
                         <div className="filter-container">
                             <div className="selector"><Filtre  setCharactersTabFiltered={setCharactersTabFiltered} select="gender" nameDisplay="Genre" /></div>
                             <div className="selector"><Filtre  setCharactersTabFiltered={setCharactersTabFiltered} select="species" nameDisplay="Espèces"/></div>
                             {/* <div className="selector"><Filtre  setCharactersTabFiltered={setCharactersTabFiltered} select="height" nameDisplay="Taille"/></div>
                             <div className="selector"><Filtre  setCharactersTabFiltered={setCharactersTabFiltered} select="mass" nameDisplay="Poids"/></div> */}
-                          
                         </div>
                     </div>
-                       <Cards charactersTabFiltered={charactersTabFiltered}/>
+                    <Cards charactersTabFiltered={charactersTabFiltered}/>
                 </div> 
-                <div className="filters-Chat">
-
-                    <ChatPage /> 
-                    <Timer></Timer>
-
-               
-                </div>
-
+                    <div className="filters-Chat">
+                        <Timer></Timer>
+                        <ChatPage /> 
+                        
+                    </div>
+                
             </div>
         )
 }
